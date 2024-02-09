8499b70fa755f609bf35cecd203ff1938242f599
Alif8454 <malif201106@gmail.com>
2024-02-09 11:29:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/vebto/bedrive/Application.kt
@@ -0,0 +1,15 @@
+package com.vebto.bedrive
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+
+    }
+}
